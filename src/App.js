102bb7e85ff11a7a928f20c6970131fa375a4e34
phamdinhbinh <phamdinhbinhonly1@gmail.com>
2023-02-10 16:53:51 +0700

    bt2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { useState } from 'react';
+import styles from "./styles.module.css"; 
 
 function App() {
+  const [setDays, setSetDays] = useState('day');
+  const handleClickDay = () => {
+    setSetDays('day')
+  };
+  const handleClickNight = () => {
+    setSetDays('night')
+  };
+  const style = (setDays=== 'day' ) ? styles.Day : styles.Night ;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <button onClick={handleClickDay} value={'day'}>day</button>
+       <button onClick={handleClickNight} value={'night'}>night</button>
+      
+      <div className={style}>
+       
+      </div>
+      
     </div>
   );
 }
